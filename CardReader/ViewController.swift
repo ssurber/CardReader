26299316a510a4c0f7b88c54e246b861138473c1
Adam Surber <adam.surber@pearson.com>
2015-10-01 11:37:30 -0600

    Initial Commit

---
+++ CardReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardReader
+//
+//  Created by Surber, Seth on 10/1/15.
+//  Copyright (c) 2015 Surber, Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
