dc21d37e67cf82d16bbae4eb3b31f713038b7e1a
Seth Adam Surber <ssurber@gmail.com>
2015-10-02 13:33:28 -0600

    mostly working

--- CardReader/ViewController.swift
+++ CardReader/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
     
     @IBOutlet var imageView : UIImageView!
+    @IBOutlet var textView : UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,13 +42,83 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func openMap() {
-
+        
+//        mapItem.openInMapsWithLaunchOptions(nil)
+        
+        //Apple Maps
+        let address = textView.text.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+        let url = "http://maps.apple.com/?address="+address!
+        if (UIApplication.sharedApplication().canOpenURL(NSURL(string:"http://maps.apple.com")!)) {
+            UIApplication.sharedApplication().openURL(NSURL(string:url)!)
+        } else {
+            NSLog("Can't use Apple Maps");
+        }
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         let temp : UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = temp
-        self.dismissViewControllerAnimated(true, completion:{})
+
+        let scaledImage = scaleImage(temp, maxDimension: 640)
+        
+//        addActivityIndicator()
+
+        self.dismissViewControllerAnimated(true, completion:{
+            self.performImageRecognition(scaledImage)
+            self.openMap()
+        })
+
+    }
+    
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+    
+    func performImageRecognition(image: UIImage) {
+        // 1
+        let tesseract = G8Tesseract()
+        
+        // 2
+        tesseract.language = "eng+fra"
+        
+        // 3
+        tesseract.engineMode = .TesseractCubeCombined
+        
+        // 4
+        tesseract.pageSegmentationMode = .Auto
+        
+        // 5
+        tesseract.maximumRecognitionTime = 60.0
+        
+        // 6
+        tesseract.image = image.g8_blackAndWhite()
+        tesseract.recognize()
+        
+        // 7
+        textView.text = tesseract.recognizedText
+        textView.editable = true
+        
+        // 8
+//        removeActivityIndicator()
     }
 
 }
